cd63f37315b716c54d4aae630b7ace2bb4ea3640
Nicolas JACOB-PERES <nicolasjp05@gmail.com>
2024-04-25 17:36:58 +0200

    upgrade button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,24 +3,25 @@ import { NavLink } from "react-router-dom"
 const Navbar = () => {
   return (
     <header className="header">
-        <NavLink to="/" className="w-10 h-10 rounded-lg bg-blue-500 items-center justify-center flex font-bold shadow-md">
-            <h2 className="sm:text-sm sm:leading-snug text-center neo-brutalism-blue
+        <NavLink to="/" className={({isActive}) => isActive ?
+            'neo-brutalism-blue font-bold' : 'neo-brutalism-blue2'}>
+            <h2 className="sm:text-sm sm:leading-snug text-center
                 py-3 px-5 text-white mx-5">
                 NJP
             </h2>
         </NavLink>
         <nav className="flex text-lg gap-7 font-medium">
             <NavLink to='/about' className={({isActive}) => isActive ?
-            'text-white' : 'text-black'}>
-                <h2 className="sm:text-sm sm:leading-snug text-center neo-brutalism-blue
-                py-3 px-5 mx-5">
+            'neo-brutalism-blue font-bold' : 'neo-brutalism-blue2'}>
+                <h2 className="sm:text-sm sm:leading-snug text-center 
+                py-3 px-5 text-white mx-5">
                     About
                 </h2>
             </NavLink>
             <NavLink to='/projects' className={({isActive}) => isActive ?
-            'text-white' : 'text-black'}>
-                <h2 className="sm:text-sm sm:leading-snug text-center neo-brutalism-blue
-                py-3 px-5 mx-5">
+            'neo-brutalism-blue font-bold' : 'neo-brutalism-blue2'}>
+                <h2 className="sm:text-sm sm:leading-snug text-center
+                py-3 px-5 text-white mx-5">
                         Projects
                 </h2>
             </NavLink>
@@ -30,3 +31,4 @@ const Navbar = () => {
 }
 
 export default Navbar
+
