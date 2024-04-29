4cd2600140b04169a06f304b9afd9cac9a71b2de
Nicolas JACOB-PERES <nicolasjp05@gmail.com>
2024-04-29 12:24:52 +0200

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,10 @@ const App = () => {
         <Router>
             <Navbar/>
             <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/about" element={<About/>}/>
-                <Route path="/projects" element={<Projects/>}/>
-                <Route path="/contact" element={<Contact/>}/>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/about" element={<About/>}/>
+              <Route path="/projects" element={<Projects/>}/>
+              <Route path="/contact" element={<Contact/>}/>
             </Routes>
         </Router>
     </main>
