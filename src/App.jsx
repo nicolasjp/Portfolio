592696766f33fee81bc1a03df8cae7e4605cbf7c
Nicolas JACOB-PERES <nicolasjp05@gmail.com>
2024-04-29 12:39:01 +0200

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import {Route, HashRouter as Router, Routes} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import {Home, About, Projects, Contact} from './pages';
 
