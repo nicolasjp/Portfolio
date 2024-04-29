4c86f49a1d7e791d501932fdba576baabd9feb15
Nicolas JACOB-PERES <nicolasjp05@gmail.com>
2024-04-29 12:27:59 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-  base: './Portfolio/',
+  base: './',
   root: 'src/',
   publicDir: '../public/',
   build: {
