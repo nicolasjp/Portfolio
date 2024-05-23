1f7000cfd237887cafbbe521be62cf0ebf4c5b45
Nicolas JACOB-PERES <nicolasjp05@gmail.com>
2024-05-24 00:00:19 +0200

    fix smartphone size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,30 +3,18 @@ import { NavLink } from "react-router-dom"
 const Navbar = () => {
   return (
     <header className="header">
-        <NavLink to="/" className={({isActive}) => isActive ?
-            'neo-brutalism-blue font-bold' : 'neo-brutalism-blue2'}>
-            <h2 className="sm:text-sm sm:leading-snug text-center
-                py-3 px-5 text-white mx-5">
-                NJP
-            </h2>
+    <NavLink to="/" className={({isActive}) => isActive ? 'neo-brutalism-blue font-bold' : 'neo-brutalism-blue2'}>
+        <h2 className="sm:text-sm sm:leading-snug text-center py-3 px-5 text-white mx-5">NJP</h2>
+    </NavLink>
+    <nav className="flex text-lg gap-7 font-medium">
+        <NavLink to='/about' className={({isActive}) => isActive ? 'neo-brutalism-blue font-bold' : 'neo-brutalism-blue2'}>
+            <h2 className="sm:text-sm sm:leading-snug text-center py-3 px-5 text-white mx-5">About</h2>
         </NavLink>
-        <nav className="flex text-lg gap-7 font-medium">
-            <NavLink to='/about' className={({isActive}) => isActive ?
-            'neo-brutalism-blue font-bold' : 'neo-brutalism-blue2'}>
-                <h2 className="sm:text-sm sm:leading-snug text-center 
-                py-3 px-5 text-white mx-5">
-                    About
-                </h2>
-            </NavLink>
-            <NavLink to='/projects' className={({isActive}) => isActive ?
-            'neo-brutalism-blue font-bold' : 'neo-brutalism-blue2'}>
-                <h2 className="sm:text-sm sm:leading-snug text-center
-                py-3 px-5 text-white mx-5">
-                        Projects
-                </h2>
-            </NavLink>
-        </nav>
-    </header>
+        <NavLink to='/projects' className={({isActive}) => isActive ? 'neo-brutalism-blue font-bold' : 'neo-brutalism-blue2'}>
+            <h2 className="sm:text-sm sm:leading-snug text-center py-3 px-5 text-white mx-5">Projects</h2>
+        </NavLink>
+    </nav>
+</header>
   )
 }
 
